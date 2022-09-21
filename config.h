5a757208ca4acbb1f835d4cbc58be4ca12293045
fredocli <frederic.terrible@gmail.com>
2022-09-21 22:22:54 +0200

    update

--- config.h
+++ config.h
@@ -1,6 +1,8 @@
-/* See LICENSE file for copyright and license details. */
+/* this Fredo config base on ls */
+
 
 /* Constants */
+
 #define TERMINAL "alacritty"
 #define TERMCLASS "St"
 
@@ -69,10 +71,10 @@ static int resizehints = 0;    /* 1 means respect size hints in tiled resizals *
 #include "vanitygaps.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[T]",      tile },    /* first entry is default */
+	{ "[M]",      monocle }, /* first entry is default */
+	{ "[MM]",	  deck },	
+	{ "[T]",      tile },   
 	{ "TTT",      bstack },
-	{ "[M]",      monocle },
-	{ "[MM]",	deck },			
 	{ "[O]",      centeredmaster },
 	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ NULL,		NULL },
@@ -102,17 +104,17 @@ static const char *termcmd[]  = { TERMINAL, NULL };
  */
 ResourcePref resources[] = {
 		{ "borderpx",		INTEGER, &borderpx },
-		{ "snap",		INTEGER, &snap },
+		{ "snap",			INTEGER, &snap },
 		{ "showbar",		INTEGER, &showbar },
-		{ "topbar",		INTEGER, &topbar },
+		{ "topbar",			INTEGER, &topbar },
 		{ "nmaster",		INTEGER, &nmaster },
 		{ "resizehints",	INTEGER, &resizehints },
-		{ "mfact",		FLOAT,	&mfact },
-		{ "gappih",		INTEGER, &gappih },
-		{ "gappiv",		INTEGER, &gappiv },
-		{ "gappoh",		INTEGER, &gappoh },
-		{ "gappov",		INTEGER, &gappov },
-		{ "swallowfloating",	INTEGER, &swallowfloating },
+		{ "mfact",			FLOAT,	&mfact },
+		{ "gappih",			INTEGER, &gappih },
+		{ "gappiv",			INTEGER, &gappiv },
+		{ "gappoh",			INTEGER, &gappoh },
+		{ "gappov",			INTEGER, &gappov },
+		{ "swallowfloating",INTEGER, &swallowfloating },
 		{ "smartgaps",		INTEGER, &smartgaps },
 };
 
@@ -124,53 +126,70 @@ static Key keys[] = {
 	STACKKEYS(MODKEY,                          focus)
 	STACKKEYS(MODKEY|ShiftMask,                push)
 
-	TAGKEYS(			XK_F1,		0)
-	TAGKEYS(			XK_F2,		1)
-	TAGKEYS(			XK_F3,		2)
-	TAGKEYS(			XK_F4,		3)
-	{ MODKEY,			XK_F6,		spawn,		SHCMD("xbacklight -dec 200;kill -44 $(pidof dwmblocks)") },
-	{ MODKEY,			XK_F7,		spawn,		SHCMD("xbacklight -inc 200;kill -44 $(pidof dwmblocks)") },
-	{ MODKEY,			XK_F9,		spawn,		SHCMD("pamixer --allow-boost -d 10; kill -44 $(pidof dwmblocks)") },
-	{ MODKEY,			XK_F10,		spawn,		SHCMD("pamixer --allow-boost -i 10; kill -44 $(pidof dwmblocks)") },
-	{ MODKEY,			XK_space,	zoom,		{0} },
-	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },
+	TAGKEYS(				XK_F1,		0)
+	TAGKEYS(				XK_F2,		1)
+	TAGKEYS(				XK_F3,		2)
+	TAGKEYS(				XK_F4,		3)
 
-		
-	{ MODKEY,			XK_t,		setlayout,	{.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,		XK_t,		setlayout,	{.v = &layouts[1]} },
-	{ MODKEY,			XK_m,		setlayout,	{.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,		XK_m,		setlayout,	{.v = &layouts[3]} },
- 	{ MODKEY,			XK_o,		setlayout,	{.v = &layouts[4]} },
+	{ MODKEY,				XK_F6,		spawn,		SHCMD("xbacklight -dec 200;kill -44 $(pidof dwmblocks)") },
+	{ MODKEY,				XK_F7,		spawn,		SHCMD("xbacklight -inc 200;kill -44 $(pidof dwmblocks)") },
+	{ MODKEY,				XK_F9,		spawn,		SHCMD("pamixer --allow-boost -d 10; kill -44 $(pidof dwmblocks)") },
+	{ MODKEY,				XK_F10,		spawn,		SHCMD("pamixer --allow-boost -i 10; kill -44 $(pidof dwmblocks)") },
+	{ MODKEY,				XK_space,	zoom,		{0} },
+	{ MODKEY,				XK_f,		togglefloating,	{0} },		
+	{ MODKEY,				XK_m,		setlayout,	{.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,		XK_m,		setlayout,	{.v = &layouts[1]} },
+	{ MODKEY,				XK_t,		setlayout,	{.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,		XK_t,		setlayout,	{.v = &layouts[3]} },
+ 	{ MODKEY,				XK_o,		setlayout,	{.v = &layouts[4]} },
 
-
-	{ MODKEY,			XK_h,		setmfact,       {.f = -0.05} }, 	
-	{ MODKEY,			XK_l,		setmfact,       {.f = +0.05} },
-
-	{ MODKEY,			XK_b,		togglebar,	{0} },
-	{ MODKEY,			XK_g,		togglegaps,	{0} },
+	{ MODKEY,				XK_h,		setmfact,       {.f = -0.05} }, 	
+	{ MODKEY,				XK_l,		setmfact,       {.f = +0.05} },
+	{ MODKEY,				XK_b,		togglebar,	{0} },
+	{ MODKEY,				XK_g,		togglegaps,	{0} },
 	{ MODKEY|ShiftMask,		XK_g,		defaultgaps,	{0} },
-	{ MODKEY,			XK_q,		killclient,	{0} },
+	{ MODKEY,				XK_q,		killclient,	{0} },
 	{ MODKEY|ShiftMask,		XK_q,		quit,		{0} },
 
-	{ MODKEY,			XK_e,		spawn,          SHCMD("emoji -p") },
-	{ MODKEY|ShiftMask,		XK_e,		spawn,          SHCMD("emoji") },
-	{ MODKEY,			XK_n,		spawn,          SHCMD(TERMINAL " -e vifm") },
-	{ MODKEY|ShiftMask,		XK_n,		spawn,          SHCMD("nautilus") },
-	{ MODKEY,			XK_r,		spawn,		SHCMD("snip") },
-	{ MODKEY|ShiftMask,		XK_r,		spawn,          SHCMD("rofi -show drun") },
-	{ MODKEY,			XK_v,		spawn,		SHCMD("clipmenuw") },
-	{ MODKEY|ShiftMask,		XK_v,		spawn,		SHCMD("clipmenu") },
-	{ MODKEY,			XK_w,		spawn,		SHCMD("$BROWSER") },
-	{ MODKEY|ShiftMask,		XK_w,		spawn,		SHCMD("$BROWSER2") },
-	{ MODKEY,			XK_y,		spawn,		SHCMD("passmenu") },
-	{ MODKEY|ShiftMask,		XK_y,		spawn,		SHCMD("passmenu --type") },
-	{ MODKEY,			XK_Return,	spawn,		{.v = termcmd } },
+	//{ MODKEY,				XK_e,		spawn,          SHCMD("emoji -p") },
+	//{ MODKEY|ShiftMask,		XK_e,		spawn,          SHCMD("emoji") },
+	//{ MODKEY,				XK_n,		spawn,          SHCMD(TERMINAL " -e vifm") },
+	//{ MODKEY|ShiftMask,		XK_n,		spawn,          SHCMD("nautilus") },
+	//{ MODKEY,				XK_s,		spawn,			SHCMD("snip") },
+	//{ MODKEY|ShiftMask,		XK_s,		spawn,			SHCMD("systemctl suspend") },
+	//{ MODKEY,				XK_r,		spawn,          SHCMD("rofi -show drun") },
+	//{ MODKEY,				XK_v,		spawn,			SHCMD("clipmenuw") },
+	//{ MODKEY|ShiftMask,		XK_v,		spawn,			SHCMD("clipmenu") },
+	//{ MODKEY,				XK_w,		spawn,			SHCMD("$BROWSER") },
+	//{ MODKEY|ShiftMask,		XK_w,		spawn,			SHCMD("$BROWSER2") },
+	//{ MODKEY,				XK_y,		spawn,			SHCMD("passmenu") },
+	//{ MODKEY|ShiftMask,		XK_y,		spawn,			SHCMD("passmenu --type") },
+	//{ MODKEY,				XK_Return,	spawn,		{.v = termcmd } },
+
+	{ 0, 					XF86XK_AudioMute,			spawn,			SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },					// volume mute
+	{ 0, 					XF86XK_AudioRaiseVolume,	spawn,			SHCMD("pamixer --allow-boost -i 3; kill -44 $(pidof dwmblocks)") },	// volume increase
+	{ 0, 					XF86XK_AudioLowerVolume,	spawn,			SHCMD("pamixer --allow-boost -d 3; kill -44 $(pidof dwmblocks)") }, // volume decrease
+	{ 0, 					XF86XK_AudioPrev,			spawn,		{.v = (const char*[]){ "mpc", "prev", NULL } } },
+	{ 0, 					XF86XK_AudioNext,			spawn,		{.v = (const char*[]){ "mpc",  "next", NULL } } },
+	{ 0, 					XF86XK_AudioPause,			spawn,		{.v = (const char*[]){ "mpc", "toggle", NULL } } },
+	{ 0, 					XF86XK_AudioPlay,			spawn,		{.v = (const char*[]){ "mpc", "toggle", NULL } } },
+	{ 0, 					XF86XK_AudioStop,			spawn,		{.v = (const char*[]){ "mpc", "toggle", NULL } } },
+	{ 0, 					XF86XK_AudioRewind,			spawn,		{.v = (const char*[]){ "mpc", "seek", "-10", NULL } } },
+	{ 0, 					XF86XK_AudioForward,		spawn,		{.v = (const char*[]){ "mpc", "seek", "+10", NULL } } },
+
 
 
 };
 
 /* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* click can be 
+
+ClkLtSymbol, 
+
+
+ClkWinTitle, 
+
+, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 #ifndef __OpenBSD__
@@ -182,14 +201,22 @@ static Button buttons[] = {
 	{ ClkStatusText,        0,              Button5,        sigdwmblocks,   {.i = 5} },
 	{ ClkStatusText,        ShiftMask,      Button1,        sigdwmblocks,   {.i = 6} },
 #endif
-	{ ClkStatusText,        MODKEY,		Button1,        spawn,          SHCMD(TERMINAL " -e vim ~/.local/src/dwm/config.h") },
 
+	// ClkWinTitle
+	{ ClkWinTitle,        MODKEY,		Button1,        spawn,          SHCMD("notify-send title") },
+
+	// ClkStatusText partie grise avec les icons
+	{ ClkStatusText,        MODKEY,		Button1,        spawn,          SHCMD(TERMINAL " -e vim ~/.repo/dwm/config.h") },
+	{ ClkStatusText,        MODKEY,		Button2,        spawn,          SHCMD("code ~/.repo/dwm/") },
+	
+	// ClkClientWin la fenetre du programe
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        defaultgaps,	{0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkClientWin,		MODKEY,		Button4,	incrgaps,	{.i = +1} },
 	{ ClkClientWin,		MODKEY,		Button5,	incrgaps,	{.i = -1} },
 
+	// ClkTagBar parite 1 2 3 
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
@@ -197,6 +224,7 @@ static Button buttons[] = {
 	{ ClkTagBar,		0,		Button4,	shiftview,	{.i = -1} },
 	{ ClkTagBar,		0,		Button5,	shiftview,	{.i = 1} },
 
+	// ClkRootWin ??
 	{ ClkRootWin,		0,		Button2,	togglebar,	{0} },
 };
 
